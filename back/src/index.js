903ab16bc9ba8afb625802ca7036324a346100bd
AlexSangronis <sangronisalexandra97@gmail.com>
2022-12-14 16:13:06 -0300

    cors-2

--- back/src/index.js
+++ back/src/index.js
@@ -3,23 +3,19 @@ const express = require("express");
 const morgan = require("morgan");
 const { respuesta, envioMensaje } = require("./utils/handleMail.js");
 const app = express();
-const cors = require("cors");
 const DEPLOY_FRONT = process.env.DEPLOY_FRONT;
 
 app.use(express.json({ limit: "50mb" }));
-app.use(
-  express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 })
-);
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(morgan("dev"));
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", `${DEPLOY_FRONT}`); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Origin: `${DEPLOY_FRONT}`"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials: true");
   res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept"
   );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
 

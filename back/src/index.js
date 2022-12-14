d08607ae00f231de5671da59d29bcb47d89bf65a
AlexSangronis <sangronisalexandra97@gmail.com>
2022-12-14 14:34:59 -0300

    eliminando socket

--- back/src/index.js
+++ back/src/index.js
@@ -1,25 +1,10 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
-const { Server } = require("socket.io");
-const http = require("http");
 const { respuesta, envioMensaje } = require("./utils/handleMail.js");
 const app = express();
 const cors = require("cors");
 const DEPLOY_FRONT = process.env.DEPLOY_FRONT;
-
-//io//
-/* const servers = http.createServer(app);
-const io = new Server(servers, {
-  cors: {
-    origin: [
-      "http://localhost:5173",
-      "http://127.0.0.1:5173",
-      `${DEPLOY_FRONT}`,
-    ],
-    credentials: true,
-  },
-});  */
 
 app.use(express.json({ limit: "50mb" }));
 app.use(
@@ -56,20 +41,6 @@ app.use((error, req, res) => {
   // console.error(error);
   return res.send(name + message);
 });
-
-/* io.on("connection", (socket) => {
-  socket.on("message", async (message) => {
-    if (Object.keys(message).length > 0) {
-      let html = `
-        <b>Your message: "${message.mensaje}" was sent correctly<b/>
-        `;
-      await respuesta(message.mail, "successful delivery confirmation!", html);
-      await envioMensaje(message.mail, message.nombre, message.mensaje);
-      return;
-    }
-    console.log("no hay nada");
-  });
-}); */
 
 const PORT = 3001;
 app.listen(PORT, () => {

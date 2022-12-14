5eedcdc1b504e4ced6cd7e9afc88bff5b0d82cfd
AlexSangronis <sangronisalexandra97@gmail.com>
2022-12-14 14:22:32 -0300

    de socket a express

--- back/src/index.js
+++ back/src/index.js
@@ -9,7 +9,7 @@ const cors = require("cors");
 const DEPLOY_FRONT = process.env.DEPLOY_FRONT;
 
 //io//
-const servers = http.createServer(app);
+/* const servers = http.createServer(app);
 const io = new Server(servers, {
   cors: {
     origin: [
@@ -19,7 +19,7 @@ const io = new Server(servers, {
     ],
     credentials: true,
   },
-});
+});  */
 
 app.use(express.json({ limit: "50mb" }));
 app.use(
@@ -36,12 +36,28 @@ app.use(
     credentials: true,
   })
 );
-
-app.use((err, req, res) => {
-  res.send(err);
+app.get("/", (req, res) => {
+  res.send("hola");
+});
+app.post("/", async (req, res) => {
+  const { nombre, asunto, mail, mensaje } = req.body;
+  console.log(res.body);
+  let html = `
+        <b>Your message: "${mensaje}" was sent correctly<b/>
+        `;
+  await respuesta(mail, "successful delivery confirmation!", html);
+  await envioMensaje(mail, nombre, mensaje);
+  return res.send("exitoso");
 });
 
-io.on("connection", (socket) => {
+app.use((error, req, res) => {
+  const name = error.name;
+  const message = error.message;
+  // console.error(error);
+  return res.send(name + message);
+});
+
+/* io.on("connection", (socket) => {
   socket.on("message", async (message) => {
     if (Object.keys(message).length > 0) {
       let html = `
@@ -53,9 +69,9 @@ io.on("connection", (socket) => {
     }
     console.log("no hay nada");
   });
-});
+}); */
 
 const PORT = 3001;
-servers.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`); // eslint-disable-line no-console
 });
